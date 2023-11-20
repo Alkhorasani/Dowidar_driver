5aa16ef913e570585420180b4dff2cf909ba0b46
iamum <iam.umairimran@gmail.com>
2023-11-20 21:08:24 +0500

    ui update , fcm

---
+++ lib/ClassModules/cm_StringConstants/cm_StringConstantsVwLogin.dart
@@ -0,0 +1,6 @@
+class cm_StringConstantsVwLogin {
+  static String? strEnterCred = 'Enter credentials for login';
+  static String? strPhoneNumber = 'Phone number';
+  static String? strPassword = 'Password';
+  static String? strLogin = 'Login';
+}
